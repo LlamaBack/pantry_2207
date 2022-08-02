b1ebca4f8e8f5f680b5e98944f64363dd97f5e4f
Jake Kim <imjakekim@gmail.com>
2022-08-02 10:37:20 -0700

    finished iteration 4

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,8 +1,9 @@
 class CookBook
-  attr_reader :recipes, :ingredients
+  attr_reader :recipes, :ingredients, :date
   def initialize
     @recipes = []
     @ingredients = []
+    @date = Time.now.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
@@ -17,5 +18,35 @@ class CookBook
 
   def highest_calorie_meal
     fattest_recipe = @recipes.max { |a, b| a.total_calories <=> b.total_calories }
+  end
+
+  def summary
+    sum = []
+    @recipes.each do |recipe|
+      entry = {}
+      entry[:name] = recipe.name
+      entry[:details] = {}
+      entry[:details][:ingredients] = []
+      recipe.ingredients_required.each do |ingredient, quantity|
+        piece = {}
+        piece[:ingredient] = ingredient.name
+        piece[:amount] = "#{quantity} #{ingredient.unit}"
+        piece[:calories] = ingredient.calories * quantity
+        entry[:details][:ingredients] << piece
+      end
+
+      entry[:details][:ingredients] = entry[:details][:ingredients].sort_by{|piece| piece[:calories]}.reverse!
+      n_array = []
+      entry[:details][:ingredients].map do |piece|
+        piece.delete(:calories)
+        n_array << piece
+      end
+
+      entry[:details][:ingredients] = n_array
+
+      entry[:details][:total_calories] = recipe.total_calories
+      sum << entry
+    end
+    return sum
   end
 end
--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe CookBook do
   let(:ingredient1) {Ingredient.new({name: "Cheese", unit: "C", calories: 100})}
   let(:ingredient2) {Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})}
   let(:ingredient3) {Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})}
-  let(:ingredient4) {Ingredient.new({name: "Bun", unit: "g", calories: 75})}
+  let(:ingredient4) {Ingredient.new({name: "Bun", unit: "g", calories: 1})}
 
   let(:recipe1) {Recipe.new("Mac and Cheese")}
-  let(:recipe2) {Recipe.new("Cheese Burger")}
+  let(:recipe2) {Recipe.new("Burger")}
 
   let(:cookbook) {CookBook.new}
 
@@ -48,5 +48,26 @@ RSpec.describe CookBook do
     cookbook.add_recipe(recipe2)
 
     expect(cookbook.highest_calorie_meal).to eq(recipe2)
+  end
+
+  it 'records date recipe was created' do
+    expect(cookbook.date).to eq(Time.now.strftime("%m-%d-%Y"))
+  end
+
+  it 'summarizes itself' do
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 100)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    expect(cookbook.summary).to eq([{:name=>"Mac and Cheese",
+                                    :details=>{
+                                      :ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}],
+                                      :total_calories=>440}},
+                                    {:name=>"Burger",
+                                    :details=>{
+                                      :ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"},{:ingredient=>"Bun", :amount=>"100 g"}],
+                                      :total_calories=>500}}])
   end
 end
