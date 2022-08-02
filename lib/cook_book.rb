5e1b8148828b13822fd96e49acef00804ad7fc73
Jake Kim <imjakekim@gmail.com>
2022-08-02 09:10:07 -0700

    finished testing and implementing ingredients for cookbook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,10 +1,18 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes, :ingredients
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+
+    recipe.ingredients.map do |ingredient|
+      if !ingredients.include?(ingredient.name)
+        @ingredients << ingredient.name
+      end
+    end
+
   end
 end
--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -1,8 +1,15 @@
 require "./lib/cook_book"
 
 RSpec.describe CookBook do
-  let(:recipe1) {Recipe.new("Cheese Burger")}
-  let(:recipe2) {Recipe.new("Double Cheese Burger")}
+
+  let(:ingredient1) {Ingredient.new({name: "Cheese", unit: "C", calories: 100})}
+  let(:ingredient2) {Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})}
+  let(:ingredient3) {Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})}
+  let(:ingredient4) {Ingredient.new({name: "Bun", unit: "g", calories: 75})}
+
+  let(:recipe1) {Recipe.new("Mac and Cheese")}
+  let(:recipe2) {Recipe.new("Cheese Burger")}
+
   let(:cookbook) {CookBook.new}
 
   it 'exists' do
@@ -14,5 +21,18 @@ RSpec.describe CookBook do
     cookbook.add_recipe(recipe2)
 
     expect(cookbook.recipes).to eq([recipe1, recipe2])
+  end
+
+  it 'adds and records recipes' do
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 4)
+
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    expect(cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
   end
 end
